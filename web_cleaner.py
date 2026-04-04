39c645c0aa170b7ba58b12009c8d99e123fe96b4
somtocodes <chukwunomnazusomto@gmail.com>
2026-04-04 10:46:28 +0100

    Update web_cleaner.py

--- web_cleaner.py
+++ web_cleaner.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Web Data Cleaner", layout = "centered")
 st.title("Data Cleaner")
 st.write("Upload a messy CSV or Excel file, and i'll clean it for you automatically")
 
-uploaded_file = st.file_uploader("Upload a .csv or .xlsx file", type=['csv', 'xslx'])
+uploaded_file = st.file_uploader("Upload a .csv or .xlsx file", type=['csv', 'xlsx'])
 
 if uploaded_file is not None:
   try:
@@ -20,18 +20,17 @@ if uploaded_file is not None:
   except Exception as e:
     st.error(f'Something went wrong while reading the file. Is it corrupted? Error: {e}')
     st.stop()
-  
-    st.subheader('Original Data')
-    st.dataframe(df)
+    
+  st.subheader('Original Data')
+  st.dataframe(df)
   
     
-    if st.button('Clean data'):
-      cleaned_df, report = auto_clean(df)
-      st.success("Cleaning complete")
-      st.subheader("Cleaned Data")
-      st.dataframe(cleaned_df)
-
+  if st.button('Clean data'):
+    cleaned_df, report = auto_clean(df)
+    st.success("Cleaning complete")
+    st.subheader("Cleaned Data")
+    st.dataframe(cleaned_df)
     csv = cleaned_df.to_csv(index=False).encode('utf-8')
-    st.download_button(label='Download clean data', file_name="Cleaned_data.csv", mime="text/csv")
+    st.download_button(label='Download clean data',data=csv, file_name="Cleaned_data.csv", mime="text/csv")
 
       
